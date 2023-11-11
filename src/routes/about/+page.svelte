75ca3ed3e6d5719bdb3a8fcfe3c62858d8ab0c32
joemmalatesta <joemmalatesta@gmail.com>
2023-11-11 00:33:09 -0500

    does squad like their images ?

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import SectionHeader from "../../components/SectionHeader.svelte";
+</script>
+
+<svelte:head>
+    <title>About</title>
+</svelte:head>
+
+<div class="xl:mx-60 lg:mx-48 md:mx-20 flex flex-col justify-center items-center">
+    <div class="py-5">
+    <SectionHeader  title={"Meet the team"}/>
+</div>
+<div class="flex justify-center items-center gap-10 flex-wrap">
+	<img src="/team/joe.jpg" alt="" class=" xl:w-48 xl:h-48 lg:w-44 lg:h-44 w-36 h-36 object-cover rounded-full hover:shadow-lg hover:shadow-indigo-400 cursor-pointer ring-2 ring-indigo-400 ring-opacity-40" />
+	<img src="/team/amelio.jpg" alt="" class=" xl:w-48 xl:h-48 lg:w-44 lg:h-44 w-36 h-36 object-cover rounded-full hover:shadow-lg hover:shadow-indigo-400 cursor-pointer ring-2 ring-indigo-400 ring-opacity-40" />
+	<img src="/team/jacob.avif" alt="" class=" xl:w-48 xl:h-48 lg:w-44 lg:h-44 w-36 h-36 object-cover rounded-full hover:shadow-lg hover:shadow-indigo-400 cursor-pointer ring-2 ring-indigo-400 ring-opacity-40" />
+	<img src="/team/halee.jpg" alt="" class=" xl:w-48 xl:h-48 lg:w-44 lg:h-44 w-36 h-36 object-cover rounded-full hover:shadow-lg hover:shadow-indigo-400 cursor-pointer ring-2 ring-indigo-400 ring-opacity-40" />
+	<img src="/team/grot.jpg" alt="" class=" xl:w-48 xl:h-48 lg:w-44 lg:h-44 w-36 h-36 object-cover rounded-full hover:shadow-lg hover:shadow-indigo-400 cursor-pointer ring-2 ring-indigo-400 ring-opacity-40" />
+    <img src="/team/bethany.avif" alt="" class=" xl:w-48 xl:h-48 lg:w-44 lg:h-44 w-36 h-36 object-cover rounded-full hover:shadow-lg hover:shadow-indigo-400 cursor-pointer ring-2 ring-indigo-400 ring-opacity-40" />
+	<img src="/team/chris.jpg" alt="" class=" xl:w-48 xl:h-48 lg:w-44 lg:h-44 w-36 h-36 object-cover rounded-full hover:shadow-lg hover:shadow-indigo-400 cursor-pointer ring-2 ring-indigo-400 ring-opacity-40" />
+	<img src="/team/dom.avif" alt="" class=" xl:w-48 xl:h-48 lg:w-44 lg:h-44 w-36 h-36 object-cover rounded-full hover:shadow-lg hover:shadow-indigo-400 cursor-pointer ring-2 ring-indigo-400 ring-opacity-40" />
+	<img src="/team/josh.avif" alt="" class=" xl:w-48 xl:h-48 lg:w-44 lg:h-44 w-36 h-36 object-cover rounded-full hover:shadow-lg hover:shadow-indigo-400 cursor-pointer ring-2 ring-indigo-400 ring-opacity-40" />
+	<img src="/team/noah.jpg" alt="" class=" xl:w-48 xl:h-48 lg:w-44 lg:h-44 w-36 h-36 object-cover rounded-full hover:shadow-lg hover:shadow-indigo-400 cursor-pointer ring-2 ring-indigo-400 ring-opacity-40" />
+    <img src="/team/sam.jpg" alt="" class=" xl:w-48 xl:h-48 lg:w-44 lg:h-44 w-36 h-36 object-cover rounded-full hover:shadow-lg hover:shadow-indigo-400 cursor-pointer ring-2 ring-indigo-400 ring-opacity-40" />
+</div>
+</div>
